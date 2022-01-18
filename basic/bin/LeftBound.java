294d0a9684c2556d77ede58a1f7c737fa88f50aa
xiaozhuoops <xiaozhuoops@outlook.com>
2022-01-18 23:48:18 +0800

    2021-12-18

--- basic/bin/LeftBound.java
+++ basic/bin/LeftBound.java
@@ -6,21 +6,19 @@ public class LeftBound {
      */
     int left_bound2(int[] nums, int target) {
         int left = 0, right = nums.length, mid = 0;
-        while(left<right){
-            mid = (left+right)/2;
-            if(nums[mid] == target){
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
                 right = mid; //只要找到target就一直设置为right,故最终的左侧节点一定是right
-            }
-            else if(nums[mid] < target){
-                left = mid+1;
-            }
-            else if(nums[mid] > target){
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
                 right = mid;
             }
         }
         // left == right
         return right;
-    }    
+    }
 
     /**
      * 减而治之：
@@ -29,15 +27,29 @@ public class LeftBound {
      */
     int left_bound(int[] nums, int target) {
         if (nums.length == 0) return -1;
-        
+
         int left = 0, right = nums.length - 1, mid = 0;
-        while(left < right) {
+        while (left < right) {
             mid = (left + right) >> 1;
             if (nums[mid] >= target) {
-                right = mid;
-            }            
-            else if (nums[mid] < target) {
-                left = mid + 1;
+                right = mid; //right = mid - 1 可能会造成right < left 而越界
+            } else if (nums[mid] < target) {
+                left = mid + 1; //left = mid 可能会造成死循环
+            }
+        }
+        return (nums[right] == target) ? right : -1;
+    }
+
+    int right_bound(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+
+        int left = 0, right = nums.length - 1, mid = 0;
+        mid = (left + right + 1) >> 1;
+        while (left < right) {
+            if (nums[mid] <= target) {
+                left = mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             }
         }
         return (nums[right] == target) ? right : -1;
